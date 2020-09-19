d2fab63de58450ceedb76bdb0585a322653a42f5
A1DS19 <jp06171@gmail.com>
2020-09-19 16:40:34 -0600

    header tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,12 +1,33 @@
 const puppeteer = require('puppeteer');
-test('should load new browser page', async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('localhost:3000');
 
+let browser;
+let page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+}, 99999);
+
+//cerrar browser despues de cada test
+afterEach(async () => {
+  await browser.close();
+});
+
+test('header text should be "Blogster"', async () => {
   //revisa texto de header en DOM
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
-
-  //asercion que el texto sea = Blogster
+  //se espera que el logo del header sea = Blogster
   expect(text).toEqual('Blogster');
-}, 99999);
+});
+
+test('clicking login starts oauth flow', async () => {
+  //hace click en login con google clase = ".right a"
+  await page.click('.right a');
+
+  //tomar url de google.accounts
+  const url = await page.url();
+
+  //regex para solo tomar esa parte del url de auth
+  expect(url).toMatch(/accounts\.google\.com/);
+});
