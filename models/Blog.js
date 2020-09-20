5a26920d812ef9aa6cccb95c8c855f5492005a93
A1DS19 <jp06171@gmail.com>
2020-09-20 17:25:45 -0600

    nuevo espacio de imageUrl en db

--- models/Blog.js
+++ models/Blog.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const blogSchema = new Schema({
+  //Solo el nombre del archivo, no todo el url
+  //Es mala practica agregar todo el url
+  imageUrl: String,
   title: String,
   content: String,
   createdAt: { type: Date, default: Date.now },
-  _user: { type: Schema.Types.ObjectId, ref: 'User' }
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 mongoose.model('Blog', blogSchema);
